726cef0d39e23b3f84f74db77d7d1bc4a0cb741a
Ayan De <deayan252@gmail.com>
2025-03-25 17:12:59 +0530

    "file writeable stream is created"

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const net = require("net");
+const fs = require("node:fs/promises");
 
 const server = net.createConnection(() => {});
 
 server.on("connection", (socket) => {
   console.log("New Connection!");
+
+  socket.on("data", async (data) => {
+    //file is opened
+    const fileHandle = await fs.open(`storage/test.txt`, "w");
+    //Read from the socket and write to the stream
+    //creating stream of this fileHandle
+    const fileStream = fileHandle.createWriteStream();
+    fileStream.write(data);
+  });
 });
 
 server.listen("5000", "::1", () => {
