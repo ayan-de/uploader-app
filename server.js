93e40d7d7a403ed98df44babfd98a591c5ad3e98
Ayan De <deayan252@gmail.com>
2025-03-25 22:32:17 +0530

    "fs.open only once and not everytime when we get data"

--- server.js
+++ server.js
@@ -9,13 +9,21 @@ server.on("connection", (socket) => {
   console.log("New Connection!");
 
   socket.on("data", async (data) => {
-    //file is opened
-    fileHandle = await fs.open(`storage/test.txt`, "w");
-    //Read from the socket and write to the stream
-    //creating stream of this fileHandle
-    fileStream = fileHandle.createWriteStream();
-    //writing to our destination file
-    fileStream.write(data);
+    //if-else added to make sure that the file is opened only
+    //once when the connection is established and started getting data
+    //not everytime when we get data
+    if (!fileHandle) {
+      //file is opened
+      fileHandle = await fs.open(`storage/test.txt`, "w");
+      //Read from the socket and write to the stream
+      //creating stream of this fileHandle
+      fileStream = fileHandle.createWriteStream();
+      //writing to our destination file
+      fileStream.write(data);
+    } else {
+      //writing to our destination file
+      fileStream.write(data);
+    }
   });
 
   socket.on("end", () => {
